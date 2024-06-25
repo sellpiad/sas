24d0e4785ce8f9ff99fdef938597a47c1631efec
sellpiad <sellpiad@gmail.com>
2024-06-25 20:06:58 +0900

    first version

--- server/src/main/java/com/sas/server/RedisConfig.java
+++ server/src/main/java/com/sas/server/RedisConfig.java
@@ -1,7 +1,6 @@
 package com.sas.server;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
