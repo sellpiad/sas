cfa2c26e3ee370a9f602ae4dbdcc675e3107cfe2
sellpiad <sellpiad@gmail.com>
2024-07-30 20:46:34 +0900

    chore: docker 관련 파일 수정 및 추가 fix: 프록시 관련 -Redis 관련 설정 추가 -Mysql 관련 설정 추가 -Dockerize로 DB가 실행된 후 백엔드가 실행되도록 설정. -프론트 부분에 개발환경과 docker환경에 관련된 파일 추가. -도커 환경에서 프록시 부분에서 포트 충돌하던 현상 해결.

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,13 +2,15 @@ const {createProxyMiddleware} = require("http-proxy-middleware")
 
 
 module.exports = function(app) {
+
+  const apiTarget = process.env.REACT_APP_API_TARGET || 'http://127.0.0.1:8080'; // 기본값으로 로컬 호스트 설정
+
   app.use(
     "/api",
     createProxyMiddleware({
-      target: 'http://localhost:8080',	
+      target: apiTarget,	
       changeOrigin: true,
     })
   );
 
-  app.listen(3000);
 };
