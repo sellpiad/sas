b71ef61cb761c142c4159730b13817881ac497a8
sellpiad <sellpiad@gmail.com>
2024-10-29 12:32:39 +0900

    test

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # React 서버 시작
-pm2 serve start /home/ubuntu/sas/frontend 3000 --spa --name sas-frontend
+pm2 serve /home/ubuntu/sas/frontend/build 3000 --spa --name sas-frontend
 
 # Java 서버 시작
-nohup java -jar /home/ubuntu/sas/backend/sas-server-1.0.0.jar > /dev/null 2>&1 &
+pm2 start /home/ubuntu/sas/backend/backend.jar 8080 --name sas-backend --interpreter java
