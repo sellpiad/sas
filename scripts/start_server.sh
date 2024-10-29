1b06db082d8708673529aac700042e8dfb353306
sellpiad <sellpiad@gmail.com>
2024-10-29 13:22:20 +0900

    chore: 프로덕션 레벨 환경변수 제어 추가

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,4 +4,4 @@
 pm2 serve /home/ubuntu/sas/frontend/build 3000 --spa --name sas-frontend
 
 # Java 서버 시작
-pm2 start /home/ubuntu/sas/backend/backend.jar 8080 --name sas-backend --interpreter java
+pm2 start java --name sas-backend -- -jar /home/ubuntu/sas/backend/backend.jar --spring.profiles.active=prod
